abd2271496caf15437eee52429835f3dd7e71f25
flawas1 <105674788+viktorasste@users.noreply.github.com>
2023-01-02 22:58:34 +0200

    aaa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/mystyle.css">
-    <title>Document</title>
+    <title>Vežimėlių nuoma</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
