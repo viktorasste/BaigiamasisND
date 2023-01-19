7f0f66f2f4478929fb4a6fada08c5c31ac929d17
flawas1 <105674788+viktorasste@users.noreply.github.com>
2023-01-19 21:25:38 +0200

    19

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
-  <script src="jscript/calendar.js" defer></script>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
+    <script src="jscript/calendar.js" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/mystyle.css">
-    <link rel="stylesheet" href="/css/btn.css">
     <!-- <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -18,130 +17,257 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-<ul class="navbar-nav ms-auto">
-    <!-- Authentication Links -->
-    @guest
-        @if (Route::has('login'))
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-        @endif
-
-        @if (Route::has('register'))
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-        @endif
-    @else
-        <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}
-            </a> -->
-
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </li>
-    @endguest
-</ul>
-</div>
-</div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ms-auto">
+            <!-- Authentication Links -->
+            @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-bs-toggle="modal"
+                           data-bs-target="#loginModal">{{ __('Login') }}</a>
+                    </li>
+                @endif
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-bs-toggle="modal"
+                           data-bs-target="#registrationModal">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                       data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
+    </div>
 </nav>
-    <div class="nav_bar">
-        <header>
-            <img class="logo_pic" src="/images/301220021_519483296798508_1235047204078362737_n.jpg" alt="">
-            <!-- <ul>
-                <li>
-                    <a href="#">Kontaktai</a>
-                </li>
-                <li>
-                    <a href="#">Nuoma</a>
-                </li>
-                <li class="li_right">
-                    <a href="#">Apie</a>
-                </li>
-            </ul> -->
-        </header>
+
+
+<div class="nav_bar">
+    <header>
+        <img class="logo_pic" src="/images/301220021_519483296798508_1235047204078362737_n.jpg" alt="">
+        <!-- <ul>
+            <li>
+                <a href="#">Kontaktai</a>
+            </li>
+            <li>
+                <a href="#">Nuoma</a>
+            </li>
+            <li class="li_right">
+                <a href="#">Apie</a>
+            </li>
+        </ul> -->
+    </header>
+
+</div>
+<div class="main">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="{{asset('images/301220021_519483296798508_1235047204078362737_n.jpg')}}" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{asset('images/61214232_685469581887804_3222479632515203072_n.jpg')}}" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{asset('images/300642445_519483293465175_3573586539729186628_n.jpg')}}" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="{{asset('images/301220021_519483296798508_1235047204078362737_n.jpg')}}" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{asset('images/61214232_685469581887804_3222479632515203072_n.jpg')}}" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{asset('images/300642445_519483293465175_3573586539729186628_n.jpg')}}" alt="Third slide">
+    </div>
+  </div> -->
+
+</div>
+    <!-- <div class="carousel_pic">
+        <img class="carousel_vezim" src="/images/61214232_685469581887804_3222479632515203072_n.jpg" alt="">
+    </div> -->
+
+</div>
+<footer>
+
+    <div class="fa_social">
+
+        <a href="https://www.facebook.com/vezimeliunuomakelionems" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-skype"></a>
+        <a href="#" class="fa fa-instagram"></a>
 
     </div>
-        <div class="main">
-            <div class="carousel_pic">
-                <img class="carousel_vezim" src="/images/61214232_685469581887804_3222479632515203072_n.jpg" alt="">
-            </div>
-
-            <!-- <div id="form_uzsakymo" class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1"></span>
-                <input type="text" class="form-control" placeholder="Vardas Pavardė" aria-label="Username" aria-describedby="basic-addon1">
-              </div>
-
-              <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="pvz@pvz.lt" aria-label="Recipient's username" aria-describedby="basic-addon2">
-
-              </div>
-
-
-              <div class="input-group mb-3"> -->
-                <!-- <span class="input-group-text" id="basic-addon3">https://example.com/users/</span> -->
-
-                <!-- <input type="date" class="form-control" id="basic-url"
-                aria-describedby="basic-addon3">
-
-              </div>
-              <div class="input-group mb-3"> -->
-                <!-- <span class="input-group-text" id="basic-addon3">https://example.com/users/</span> -->
-
-                <!-- <input type="time" class="form-control" id="basic-url"
-                aria-describedby="basic-addon3"> -->
-
-              <!-- </div>
-
-              <div class="input-group mb-3">
-                <span class="input-group-text">$</span>
-                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-                <span class="input-group-text">.00</span>
-              </div>
-
-              <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username"> -->
-                <!-- <span class="input-group-text">@</span> -->
-                <!-- <input type="text" class="form-control" placeholder="" aria-label="Server"> -->
-              <!-- </div>
-
-              <div class="input-group">
-                <span class="input-group-text">Komentaras:</span>
-                <textarea class="form-control" aria-label="With textarea"></textarea>
-              </div>
-              <div class="input-group mb-3"> -->
-                <!-- <span class="input-group-text" id="basic-addon3">https://example.com/users/</span> -->
-
-                <!-- <input type="submit" class="form-control" id="basic-url"
-                aria-describedby="basic-addon3">
-
-              </div> -->
-        </div>
-        <footer>
-
-          <div class="fa_social">
-
-          <a href="https://www.facebook.com/vezimeliunuomakelionems" class="fa fa-facebook"></a>
-          <a href="#" class="fa fa-skype"></a>
-          <a href="#" class="fa fa-instagram"></a>
-
-        </div>
-        <div class="copyright">
+    <div class="copyright">
         <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by Viktoras :)
-        </div>
-        </footer>
+    </div>
+</footer>
 
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalTitle"
+     style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <form action="{{ route('login') }}" method="POST" style="display: contents;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="loginModalTitle">Prisijungti</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">{{ __('Email Address') }}</label>
+                        <div>
+                            <input class="form-control" type="email" id="email" name="email"
+                                   value="{{ old('email') }}" required
+                                   autocomplete="email" autofocus>
 
+                            @error('email')
+                            <span role="alert">
+                        <strong>{{ $message }}</strong>
+                        </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">{{ __('Password') }}</label>
+                        <div>
+                            <input class="form-control" type="password" id="password" name="password" required
+                                   autocomplete="current-password">
+
+                            @error('password')
+                            <span role="alert">
+                                            <strong>{{ $message }}</strong>
+                                          </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit" class="btn btn-primary mb-3">Prisijungti</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="row mb-0">
+                        <div class="col">
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+        </form>
+    </div>
+</div>
+</div>
+<div class="modal fade" id="registrationModal" tabindex="-1" aria-labelledby="registrationModalTitle"
+     style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <form action="{{ route('register') }}" method="POST" style="display: contents;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="registrationModalTitle">Registruotis</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name" class="form-label">{{ __('Name') }}</label>
+
+                        <div>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
+                                   name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                            @error('name')
+                            <span role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">{{ __('Email Address') }}</label>
+
+                        <div>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                   name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                            @error('email')
+                            <span role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="form-label">{{ __('Password') }}</label>
+
+                        <div>
+                            <input id="password" type="password"
+                                   class="form-control @error('password') is-invalid @enderror" name="password" required
+                                   autocomplete="new-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password-confirm" class="form-label">{{ __('Confirm Password') }}</label>
+                        <div>
+                            <input id="password-confirm" type="password" class="form-control"
+                                   name="password_confirmation" required autocomplete="new-password">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="row mb-0">
+                        <div class="col">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+        </form>
+    </div>
+</div>
+</div>
 
 </body>
 </html>
